4dbc32d882f818cbebd9d0f70dd422a855ffa9bf
NiiHaL <71907716+Nihal182@users.noreply.github.com>
2022-11-24 18:37:22 -0300

    SA09 ATIVIDADE DE ENCAPSULAMENTO SENAI

---
+++ SistemaBancoRural/Conta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancoRural.Contas
+{
+    public enum TipoConta { ContaCorrente, ContaPoupanca};
+    public class Conta
+    {
+        public string Num;
+        private double _saldo;
+        public TipoConta Tipo;
+
+        public Conta(TipoConta tipo, string num)
+        {
+            this.Num = num;
+            _saldo = 0;
+            this.Tipo = tipo;
+        }
+
+        public void Sacar(double valor)
+        {
+            _saldo = _saldo - valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo = _saldo + valor;
+        }
+
+        public double GetSaldo()
+        {
+            return _saldo;
+        }
+    }
+}
